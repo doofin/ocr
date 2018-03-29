ae3dfec678ac159d186d247b77dc38b364bbadd9
duyu <duych@chinamobiad.com>
2018-03-29 17:16:06 +0800

    list ok

--- ocr.py
+++ ocr.py
@@ -54,54 +54,32 @@ def img2tensor(imageNdarr_imread, labelStr):
     return normalizedImgNdarr, sparse_tuple_from([label_dense]), [normalizedImgNdarr.shape[1]]
 
 
-def train(x1sparse, y1sparse, y1len):
+def train(datalist):
     graph = tf.Graph()
     with graph.as_default():
         sink_x = tf.sparse_placeholder(tf.int32)
         sink_y = tf.placeholder(tf.float32, [None, None, num_features])  # num feature is input length?
-        # 1d array of size [batch_size]
         sink_lenth_y = tf.placeholder(tf.int32, [None])
-
         cell = tf.contrib.rnn.LSTMCell(num_hidden, state_is_tuple=True)
         stack = tf.contrib.rnn.MultiRNNCell([cell] * num_layers, state_is_tuple=True)
-        # The second output is the last state and we will no use that
         outputs, _ = tf.nn.dynamic_rnn(stack, sink_y, sink_lenth_y, dtype=tf.float32)
-
         shape = tf.shape(sink_y)
         batch_s, max_timesteps = shape[0], shape[1]
-
-        # Reshaping to apply the same weights over the timesteps
         outputs = tf.reshape(outputs, [-1, num_hidden])
-
-        # Truncated normal with mean 0 and stdev=0.1
-        # Tip: Try another initialization
-        # see https://www.tensorflow.org/versions/r0.9/api_docs/python/contrib.layers.html#initializers
         W = tf.Variable(tf.truncated_normal([num_hidden, num_classes], stddev=0.1))
-        # Zero initialization
         b = tf.Variable(tf.constant(0., shape=[num_classes]))
-
-        # Doing the affine projection
-        # Reshaping back to the original shape
-        # logits = tf.reshape(tf.matmul(outputs, W) + b, [batch_s, -1, num_classes])
         logits = tf.transpose(tf.reshape(tf.matmul(outputs, W) + b, [batch_s, -1, num_classes]), (1, 0, 2))
         loss = tf.nn.ctc_loss(sink_x, logits, sink_lenth_y)
         cost = tf.reduce_mean(loss)
         optimizer = tf.train.MomentumOptimizer(initial_learning_rate, 0.9).minimize(cost)
-        # Option 2: tf.nn.ctc_beam_search_decoder
-        # (it's slower but you'll get better results)
         decoded, log_prob = tf.nn.ctc_greedy_decoder(logits, sink_lenth_y)
         ler = tf.reduce_mean(tf.edit_distance(tf.cast(decoded[0], tf.int32), sink_x))
-        # Inaccuracy: label error rate
-
     with tf.Session(graph=graph) as sess:
         tf.global_variables_initializer().run()
         for curr_epoch in range(num_epochs):
             train_cost = train_ler = 0
             start = time.time()
-
-            # for batch in range(num_batches_per_epoch):
-            for dataset in [[x1sparse, y1sparse, y1len], [x1sparse, y1sparse, y1len]]:
-                idx=0
+            for idx,dataset in enumerate(datalist):
                 p("nth dataset")
                 print(idx)
                 feed = {sink_y: dataset[0],
@@ -111,10 +89,8 @@ def train(x1sparse, y1sparse, y1len):
                 batch_cost, _ = sess.run([cost, optimizer], feed)
                 train_cost += batch_cost * batch_size
                 train_ler += sess.run(ler, feed_dict=feed) * batch_size
-
                 train_cost /= num_examples
                 train_ler /= num_examples
-
                 val_cost, val_ler = sess.run([cost, ler], feed_dict=feed)
                 log = "Epoch {}/{}, train_cost = {:.3f}, train_ler = {:.3f}, time = {:.3f}"
                 print(log.format(curr_epoch + 1, num_epochs, train_cost, train_ler,
@@ -123,7 +99,6 @@ def train(x1sparse, y1sparse, y1len):
 
         result_dec = sess.run(decoded[0], feed_dict=feed)
         result_dense = result_dec[1]
-
         final_decoded = [characterListInUsage[i] for i in result_dense]
         print('Original:\n%s' % inputstring)
         print('Decoded:\n%s' % ''.join(final_decoded))
@@ -131,7 +106,4 @@ def train(x1sparse, y1sparse, y1len):
 
 p(list(map(lambda x: x, range(1, 10))))  # wtf
 x1, y1, y1len = img2tensor(misc.imread(inputimageName), inputstring)
-train(x1, y1, y1len)
-for x in [[1,2],[3,4]]:
-    for y in x:
-        p(y)
+train([[x1, y1, y1len],[x1, y1, y1len]])
